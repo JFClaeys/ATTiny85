f6bf60ea2afbd86e49a4b3c7ee218cd12f4675b2
Jean-Francois Claeys <jf@multidev.net>
2023-01-04 10:53:06 -0500

    Testing a WS2812B strand of 60 leds

---
+++ RainbowTest/RainbowTest.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_NeoPixel.h>
+
+
+#define PIN          PB0
+#define NUMPIXELS     60
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+  pixels.begin();
+  pixels.setBrightness(50);
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+void loop() {
+  rainbowCycle(10);
+}
